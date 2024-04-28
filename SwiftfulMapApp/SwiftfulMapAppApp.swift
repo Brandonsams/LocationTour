5c5bfd64d1537e7b8a8a941765086bbc0eea813b
brandonsams <brandonkeithsams@gmail.com>
2024-04-28 13:01:03 -0600

    Initial Commit

---
+++ SwiftfulMapApp/SwiftfulMapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulMapAppApp.swift
+//  SwiftfulMapApp
+//
+//  Created by Brandon Sams on 4/28/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulMapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
