9f3cb4fa1b88bfff317860a46f3853766fdabdc4
brandonsams <brandonkeithsams@gmail.com>
2024-04-28 20:26:58 -0600

    Initial commit

---
+++ SwiftfulMapApp/ViewModels/LocationsViewModel.swift
@@ -0,0 +1,86 @@
+//
+//  LocationsViewModel.swift
+//  SwiftfulMapApp
+//
+//  Created by Brandon Sams on 4/28/24.
+//
+
+import Foundation
+import MapKit
+import SwiftUI
+
+class LocationsViewModel: ObservableObject {
+    // All loaded locations
+    @Published var locations: [Location]
+
+    // Current location on the map
+    @Published var mapLocation: Location {
+        didSet {
+            updateMapRegion(location: mapLocation)
+        }
+    }
+
+    // Current region on map
+    @Published var mapRegion: MKCoordinateRegion = .init()
+    let mapSpan = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+
+    // Show list of locations
+    @Published var showLocationsList: Bool = false
+    
+    // Show location detail via sheet
+    @Published var sheetLocation: Location? = nil
+
+    init() {
+        let locations = LocationsDataService.locations
+        self.locations = locations
+        self.mapLocation = locations.first!
+        self.updateMapRegion(location: locations.first!)
+    }
+
+    private func updateMapRegion(location: Location) {
+        withAnimation(.easeInOut) {
+            mapRegion = MKCoordinateRegion(
+                center: location.coordinates,
+                span: mapSpan)
+        }
+    }
+
+    public func toggleLocationsList() {
+        withAnimation(.easeInOut) {
+            showLocationsList.toggle()
+        }
+    }
+
+    public func showNextLocation(location: Location) {
+        withAnimation(.easeInOut) {
+            mapLocation = location
+            showLocationsList = false
+        }
+    }
+
+    public func nextButtonPressed_OLD() {
+        // get current index
+        guard let currentIndex = locations.firstIndex(where: { $0 == mapLocation }) else {
+            print("Could not find current index in locations array. Should NEVER happpen.")
+            return
+        }
+
+        // check if current index is valid
+        let nextIndex = currentIndex + 1
+        guard locations.indices.contains(nextIndex) else {
+            guard let firstLocation = locations.first else { return }
+            showNextLocation(location: firstLocation)
+            return
+        }
+        let nextLocation = locations[nextIndex]
+        showNextLocation(location: nextLocation)
+    }
+
+    public func nextButtonPressed() {
+        guard let currentIndex = locations.firstIndex(of: mapLocation) else {
+            return
+        }
+        let nextIndex = (currentIndex + 1) % locations.count
+        showNextLocation(location: locations[nextIndex])
+    }
+}
---
+++ SwiftfulMapApp/Views/LocationsView.swift
@@ -0,0 +1,106 @@
+//
+//  LocationsView.swift
+//  SwiftfulMapApp
+//
+//  Created by Brandon Sams on 4/28/24.
+//
+
+import MapKit
+import SwiftUI
+
+struct LocationsView: View {
+    @EnvironmentObject private var vm: LocationsViewModel
+    let maxWidthForIpad: CGFloat = 700
+
+    var body: some View {
+        ZStack {
+            mapLayer
+                .ignoresSafeArea()
+
+            VStack(spacing: 0) {
+                header
+                    .padding()
+                    .frame(maxWidth: maxWidthForIpad)
+                Spacer()
+                locationsPreviewStack
+            }
+        }
+        .sheet(item: $vm.sheetLocation, onDismiss: nil) { location in
+            LocationDetailView(location: location)
+        }
+    }
+}
+
+#Preview {
+    LocationsView()
+        .environmentObject(LocationsViewModel())
+}
+
+extension LocationsView {
+    private var header: some View {
+        VStack {
+            Button(action: vm.toggleLocationsList) {
+                Text(vm.mapLocation.name + ", " + vm.mapLocation.cityName)
+                    .font(.title2)
+                    .fontWeight(.black)
+                    .foregroundColor(.primary)
+                    .frame(height: 55)
+                    .frame(maxWidth: /*@START_MENU_TOKEN@*/ .infinity/*@END_MENU_TOKEN@*/)
+                    .animation(.none, value: vm.mapLocation)
+                    .overlay(alignment: .leading) {
+                        Image(systemName: "arrow.down")
+                            .font(.headline)
+                            .foregroundColor(.primary)
+                            .padding()
+                            .rotationEffect(Angle(degrees: vm.showLocationsList ? 180 : 0))
+                    }
+            }
+            if vm.showLocationsList {
+                LocationsListView()
+            }
+        }
+        .background(.thickMaterial)
+        .cornerRadius(10.0)
+        .shadow(color: Color.black.opacity(0.3), radius: 20, x: 0, y: 15)
+    }
+
+    private var mapLayer: some View {
+        Map(
+            coordinateRegion: $vm.mapRegion,
+            annotationItems: vm.locations,
+            annotationContent: { location in
+                MapAnnotation(coordinate: location.coordinates) {
+                    LocationMapAnnotationView()
+                        .scaleEffect(
+                            vm.mapLocation == location ? 1 : 0.7
+                        )
+                        .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+                        .onTapGesture {
+                            vm.showNextLocation(location: location)
+                        }
+                }
+            }
+        )
+    }
+
+    private var locationsPreviewStack: some View {
+        ZStack {
+            ForEach(vm.locations) { location in
+                if vm.mapLocation == location {
+                    LocationPreviewView(location: location)
+                        .shadow(color: Color.black.opacity(0.3), radius: 20)
+                        .padding()
+                        .frame(maxWidth: maxWidthForIpad)
+                        .frame(maxWidth: .infinity)
+                        .padding(.bottom)
+                        .transition(
+                            .asymmetric(
+                                insertion: .move(edge: .trailing),
+                                removal: .move(edge: .leading)
+                            )
+                        )
+                }
+            }
+        }
+    }
+}
